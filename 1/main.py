835585574c268a41ffc15458c990daf07f31492a
stefanavra <stefan.avra@gmail.com>
2020-12-03 21:25:04 +0100

    day 1 part two

--- 1/main.py
+++ 1/main.py
@@ -3,9 +3,19 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 with open('input.txt') as f:
     lines = f.readlines()
 lines = [int(line.rstrip('\n')) for line in lines]
+# write once
 result = None
 for a in lines:
     for b in lines:
         if a + b == 2020:
-            result = (a*b)
+            result = a * b
 print(result)
+
+# copy once
+result = None
+for a in lines:
+    for b in lines:
+        for c in lines:
+            if a + b + c == 2020:
+                result = a * b * c
+print(f'part two: {result}')
