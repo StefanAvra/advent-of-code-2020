58b862b08542472696e7549a46eea7329b39febd
stefanavra <stefan.avra@gmail.com>
2020-12-06 20:57:21 +0100

    day 2 part two

--- 2/main.py
+++ 2/main.py
@@ -1,16 +1,28 @@
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def check_password(policy, pw):
-    policy_range, policy_char = policy.split(' ')
-    lowest, highest = policy_range.split('-')
-    return int(lowest) <= pw.count(policy_char) <= int(highest)
+    lowest, highest, policy_char = policy_splitter(policy)
+    return lowest <= pw.count(policy_char) <= highest
 
+def check_password_part_two(policy, pw):
+    first, second, policy_char = policy_splitter(policy)
+    print(first, second, policy_char, pw)
+    return (pw[first-1] == policy_char) != (pw[second-1] == policy_char)
+
+def policy_splitter(policy):
+    policy_range, policy_char = policy.split(' ')
+    first, second = policy_range.split('-')
+    return [int(first), int(second), policy_char]
 
 with open('input.txt') as f:
     lines = f.readlines()
 
 count_pw_ok = 0
+count_pw_ok_part_two = 0
 for line in lines:
     count_pw_ok += 1 if check_password(*line.split(': ')) else 0
+    count_pw_ok_part_two += 1 if check_password_part_two(*line.split(': ')) else 0
+    print(check_password_part_two(*line.split(': ')))
 
-print(count_pw_ok)
+
+print(count_pw_ok, count_pw_ok_part_two)
