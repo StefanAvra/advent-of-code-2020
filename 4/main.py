36ed5a2ed1e96fe4ad05e426c4308ad5d59c0560
stefanavra <stefan.avra@gmail.com>
2020-12-07 02:46:35 +0100

    day 4 part two

--- 4/main.py
+++ 4/main.py
@@ -35,7 +35,65 @@ def validator(passport):
             return False
     return True
 
+
+def validator_two(passport):
+    keys = (
+        'byr',
+        'iyr',
+        'eyr',
+        'hgt',
+        'hcl',
+        'ecl',
+        'pid',
+    )
+    for key in keys:
+        if not key in passport:
+            return False
+        else:
+            value = passport[key]
+            if key == 'byr':
+                if len(value) != 4 or not 1920 <= int(value) <= 2002:
+                    return False
+            if key == 'iyr':
+                if len(value) != 4 or not 2010 <= int(value) <= 2020:
+                    return False
+            if key == 'eyr':
+                if len(value) != 4 or not 2020 <= int(value) <= 2030:
+                    return False
+            if key == 'hgt':
+                if value[-2:] == 'cm':
+                    if not 150 <= int(value.rstrip('cm')) <= 193:
+                        return False 
+                elif value[-2:] == 'in':
+                    if not 59 <= int(value.rstrip('in')) <= 76:
+                        return False 
+                else:
+                    return False
+            if key == 'hcl':
+                if value[0] == '#':
+                    if len(value) != 7:
+                        return False
+                    for char in value[1:]:
+                        if char.lower() not in '1234567890abcdef':
+                            return False
+                else:
+                    return False
+            if key == 'ecl':
+                if value not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+                    return False
+            if key == 'pid':
+                if len(value) != 9:
+                    return False
+                try:
+                    int(value)
+                except ValueError as e:
+                    return False                
+    return True
+
+
 count_valid_passports = 0
+count_valid_passports_two = 0
 for passport in passport_parser(lines):
     count_valid_passports += 1 if validator(passport) else 0
-print(count_valid_passports)
+    count_valid_passports_two += 1 if validator_two(passport) else 0
+print(count_valid_passports, count_valid_passports_two)
