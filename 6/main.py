3234bc981eafae0438c7c6d9a543cd1eb7e84018
stefanavra <stefan.avra@gmail.com>
2020-12-10 13:30:03 +0100

    day 6 part two

--- 6/main.py
+++ 6/main.py
@@ -22,3 +22,35 @@ def get_answers():
 count_answers = [len(answers) for answers in get_answers()]
 
 print(sum(count_answers))
+
+
+def get_groups():
+    groups = []
+    group = []
+    for line in lines:
+        if line == '\n':
+            groups.append(group)
+            group = []
+        else:
+            person = line.rstrip('\n')
+            group.append(person)
+    groups.append(group)
+    return groups
+
+
+everyone_yes = []
+for group in get_groups():
+    if len(group) == 1:
+        everyone_yes.append(len(group[0]))
+    else:
+        check = group[0]
+        group_yes = 0
+        for person in group:
+            for answer in check:
+                if answer not in person:
+                    check = check.replace(answer, '')
+        group_yes += len(check)
+        everyone_yes.append(group_yes)
+
+print(sum(everyone_yes))
+
